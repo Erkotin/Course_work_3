fae7ae85dd46a8954ddcae0ccd01547082853298
EKotinov <erkotinov@gmail.com>
2023-06-22 14:06:29 +0300

    account number masked

--- src/last_operations_func.py
+++ src/last_operations_func.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import re
 
 
 def output_last_operations(data):
@@ -14,15 +15,23 @@ def output_last_operations(data):
         card_name = from_account.split()[:-1]  # Extract all words except the last one
         card_name = " ".join(card_name)  # Join the words with a space
 
-        #hide account number
+        #hide card number
         visible_digits = 6
-        card_number = from_account[-16:]
         hidden_part = "*" * (len(card_number) - visible_digits)
-        masked_account = " ".join([card_number[:visible_digits], hidden_part, card_number[-4:]])
+        masked_card = " ".join([card_number[:visible_digits], hidden_part, card_number[-4:]])
 
         to_account = item.get("to", "")
+
+        #hide account number
+        account_number = to_account[-20:]
+        account_name = to_account.split()[:-1]  # Extract all words except the last one
+        account_name = " ".join(account_name)
+        visible_digits_account = 4
+        hidden = "*" * (len(account_number) - visible_digits_account)
+        masked_account = " ".join([account_name, hidden, account_number[-4:]])
+
         amount = item["operationAmount"]["amount"]
         currency = item["operationAmount"]["currency"]["name"]
 
-        output = f"{formatted_date} {description}\n {card_name} {masked_account} -> {to_account}\n{amount} {currency}"
+        output = f"{formatted_date} {description}\n {card_name} {masked_card} -> {masked_account}\n{amount} {currency}"
         print(output)
